272505c9c2e4f3552561f0a16cf2da80a1f0ac96
AnoopMarld <154092035+AnoopMarld@users.noreply.github.com>
2025-11-06 11:29:45 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_01_c29072011471-9786-4552"
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
+}
